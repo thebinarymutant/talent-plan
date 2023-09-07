ce23308bd952be56b0ee92042810b64ebe065e12
Smit Patel <yoda@Smits-MacBook-Pro.local>
2023-09-07 17:37:31 +0530

    Project 1: part 3

---
+++ courses/rust/production/project-1/src/lib.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
+impl KvStore {
+
+    pub fn new() -> KvStore {
+        KvStore {
+            store: HashMap::new(),
+        }
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        self.store.get(&key).cloned()
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(
+            key,
+            value
+        );
+    }
+
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
+    }
+}
