86a30a5701b53b8e1fa1cefaf6f74e1414112032
Smit Patel <the1995captainh@gmail.com>
2024-12-20 05:07:13 +0530

    Building blocks 2: Exercise 3 using BSON

--- courses/rust/production/project-2/src/bin/exercise2.rs
+++ courses/rust/production/project-2/src/bin/exercise2.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::io::BufWriter;
 
-// Run using command `cargo run --bin exercise1
+// Run using command `cargo run --bin exercise2
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Direction {
